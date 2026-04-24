bca739ccdc8c7c6ad1123462bd235499e33bb720
Bryan <carvajalbryan@hotmail.com>
2026-04-24 00:35:29 -0500

    fix: images resize added

--- utils/resize-images.js
+++ utils/resize-images.js
@@ -10,12 +10,12 @@ const images = [
   {
     input: "src/assets/media/images/company-2.webp",
     name: "company-2",
-    sizes: [400, 600, 800, 1200],
+    sizes: [400, 800, 1200],
   },
   {
     input: "src/assets/media/images/img-8.webp",
     name: "img-8",
-    sizes: [300, 600, 1200],
+    sizes: [400, 600, 800],
   },
 ];
 
