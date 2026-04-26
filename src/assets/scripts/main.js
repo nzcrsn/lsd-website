451787ffa14214b6c43599ae201fb643a374cdbb
Bryan <carvajalbryan@hotmail.com>
2026-04-25 21:58:54 -0500

    fix: hero grid improved

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -59,29 +59,38 @@
 //   });
 // });
 
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import SplitText from "gsap/SplitText";
-import Lenis from "lenis";
+// import gsap from "gsap";
+// import ScrollTrigger from "gsap/ScrollTrigger";
+// import SplitText from "gsap/SplitText";
+// import Lenis from "lenis";
 
-gsap.registerPlugin(ScrollTrigger, SplitText);
+// gsap.registerPlugin(ScrollTrigger, SplitText);
 
-const lenis = new Lenis({
-  duration: 1.2,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  orientation: "vertical",
-  gestureOrientation: "vertical",
-  smoothWheel: true,
-  wheelMultiplier: 1,
-  touchMultiplier: 1,
-  autoRaf: false,
-});
+// const lenis = new Lenis({
+//   duration: 1.2,
+//   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+//   orientation: "vertical",
+//   gestureOrientation: "vertical",
+//   smoothWheel: true,
+//   wheelMultiplier: 1,
+//   touchMultiplier: 1,
+//   autoRaf: false,
+// });
 
-lenis.on("scroll", ScrollTrigger.update);
+// lenis.on("scroll", ScrollTrigger.update);
 
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
+// gsap.ticker.add((time) => {
+//   lenis.raf(time * 1000);
+// });
 
-gsap.ticker.lagSmoothing(0);
+// gsap.ticker.lagSmoothing(0);
+
 console.log("your rock");
+
+const video = document.querySelector(".video-media");
+const poster = document.querySelector(".poster");
+
+video.addEventListener("canplay", () => {
+  video.classList.add("loaded");
+  poster.remove();
+});
